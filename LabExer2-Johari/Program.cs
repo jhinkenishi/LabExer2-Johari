be3f8706693bdc27dc0dd6c29950ba01b13dd27d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-08 07:35:45 +0800

    Add project files.

---
+++ LabExer2-Johari/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExer2_Johari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Johari Macapundag                        ICT");
+            do
+            {
+
+                //I have no choice 
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                //I must handle this exeption but no according to lab exercise
+                int x = Convert.ToInt32(str);
+                Console.Write("y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                int sum = x + y;
+                Console.WriteLine("Result: {0}", sum);
+            } while (true);
+        }
+    }
+}
